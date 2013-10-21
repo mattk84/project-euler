c215c6cb7b0b7771b04dbb1804450b2568dd71ef
Ardonis <ardonis.ut@gmail.com>
2013-10-21 07:15:26 +0000

    add data file; make solution 22 independent of directory

--- solutions/022.rb
+++ solutions/022.rb
@@ -5,33 +5,38 @@ class Problem022
     #parse provided names file
     name_string = File.read(dirpath)
     name_array = name_string.split(',')
-    
+
     #sort names file alphabetically
     name_array.sort!
   end
-  
+
   def self.calculate_alpha_value(item)
     sum = 0
     item.each_byte do |i|
       sum += (i - 64) if i != 34
     end
-    
+
+    #p "The alpha value for #{item} is: #{sum}"
     sum
   end
-  
+
   def self.calculate_scores_for(array)
     sum = 0
-    
+
     array.each_index do |index|
       item = array[index]
       sum += calculate_alpha_value(item) * (index + 1)
     end
-    
+
     sum
   end
-  
+
   def self.run
-    path = 'data/names.txt'
+    this_file = File.expand_path(__FILE__) # '/.../solutions/022.rb'
+    parent_dir = File.dirname(File.dirname(this_file)) #'/.../'
+    path = File.join(parent_dir, 'data/names.txt') #'/.../data/names.txt'
+
+
     data = load_from_file path
     sum = calculate_scores_for data
   end
